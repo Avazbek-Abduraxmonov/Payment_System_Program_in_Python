cd73b01c26bf6ea2a9f6478f090dc56ead5d6e75
Abduraxmonov <126749705+Avazbek-Abduraxmonov@users.noreply.github.com>
2023-09-14 17:19:05 +0500

    tolov tizimi

--- dastur.py
+++ dastur.py
@@ -1,24 +1,3 @@
-# try:
-# 	the_end = "Dastur tugadi"
-# 	age = int(input("Yoshingizni kiriting: "))
-
-# 	if age>=18:
-# 		month = input("Oyni kiriting: ")
-# 		if month =="fevral":
-# 			day = int(input("Tug'ilgan kuningizni kiriting: "))
-# 			if day < 31 or day < 30:
-# 				print("xato")
-# 			else:
-# 				print("Notog'ri kiritdingiz")
-# 	if age<=87:
-# 		print(f"Yoshingiz 87dan oshib ketgan ekan")
-# 		print(the_end)	
-# 	elif age <0:
-# 		print("Manfiy son kiritish mumkin emas")		
-# except ValueError:
-# 	print("Xatolik\n{}".format(the_end))
-
-
 try:
     the_end = "the end"
     age = int(input("Enter the age "))
